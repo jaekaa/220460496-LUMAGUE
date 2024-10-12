eaf669207c53e6d09ae1ea4332311f168961a2c2
nikkzzzyu <nikzzzlumaguemerjoy@gmail.com>
2024-10-12 20:44:09 +0800

    Fixed Files to be more organized

--- Loop_Activity/index.php
+++ Loop_Activity/index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>PHP Loop Activity</title>
+        <title>Loop Activity</title>
     </head>
     <body>
     <?php
