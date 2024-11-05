79213b73ea176e4484ee2d4a1402a48e909af4e6
nikkzzzyu <nikzzzlumaguemerjoy@gmail.com>
2024-11-05 17:42:59 +0800

    Edited dp.php

--- booklibrary/dp.php
+++ booklibrary/dp.php
@@ -2,7 +2,7 @@
     $servername = "localhost:3306";
     $username = "root";
     $password = "8022";
-    $dbname = "booklibrary";
+    $dbname = "library";
 
     // Create Connection
     $conn = new mysqli($servername, $username, $password, $dbname);
@@ -10,4 +10,7 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+    
+    // Set charset to handle special characters
+    $conn->set_charset("utf8mb4");
 ?>
